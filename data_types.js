9eddde5d6b840a9c6b63d18d63b4cecaec0d90a7
Collins-Kareri <locoreri@gmail.com>
2020-01-29 12:14:59 +0300

    examine data_types in JS

---
+++ data_types.js
@@ -0,0 +1,23 @@
+//Scope bindings:var, let, const
+//var in action
+console.log(`Understanding var`);
+var name; // variable declaration
+name = `Chris Otta`; // variable assignment of type `String´
+console.log(name);
+var name = `Fleria Kate`;
+console.log(name);
+
+//let in action
+console.log(`Understanding let`);
+let fruit = `Mango`;
+console.log(fruit);
+fruit = `Pinneapple`;
+//Expect an exception to be thrown
+console.log(fruit);
+fruit = `Pawpaw`;
+console.log(fruit);
+
+//Const in action
+const PI = 3.142;
+console.log(PI);
+//Error or warrning upon reassignment of constant
